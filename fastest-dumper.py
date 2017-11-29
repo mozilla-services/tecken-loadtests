4636990ee22d9a1633efb71a1d9f15b7348f7777
Peter Bengtsson <mail@peterbe.com>
2017-11-29 09:56:48 -0500

    tool to measure dump_and_extract

---
+++ fastest-dumper.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+"""
+Loop over a bunch of .zip files in a directory, for each, read into
+memory and then measure how long it takes to unzip and extract it to disk.
+"""
+
+import random
+import os
+import zipfile
+import glob
+import tempfile
+import time
+import statistics
+from io import BytesIO
+
+import click
+
+
+def dump_and_extract(root_dir, file_buffer, name):
+    if name.lower().endswith('.zip'):
+        zf = zipfile.ZipFile(file_buffer)
+        zf.extractall(root_dir)
+    else:
+        raise ValueError(os.path.splitext(name)[1])
+    return root_dir
+
+
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
+        if abs(num) < 1024.0:
+            return '%3.1f%s%s' % (num, unit, suffix)
+        num /= 1024.0
+    return '%.1f%s%s' % (num, 'Yi', suffix)
+
+
+def time_fmt(t):
+    return '%.3fs' % t
+
+
+@click.command()
+@click.argument('directory', nargs=1)
+def run(directory):
+    files = glob.glob(os.path.join(directory, '*.zip'))
+    random.shuffle(files)
+    times = []
+    with tempfile.TemporaryDirectory() as tmpdir:
+        for fn in files:
+            name = os.path.basename(fn)
+            with open(fn, 'rb') as f:
+                in_memory = f.read()
+            size = len(in_memory)
+            t0 = time.time()
+            dump_and_extract(tmpdir, BytesIO(in_memory), name)
+            t1 = time.time()
+            times.append((size / (t1 - t0), size, t1 - t0))
+    times.sort()
+    speeds = []
+    for speed, size, time_ in times:
+        print(
+            sizeof_fmt(speed) + '/s',
+            '\t',
+            sizeof_fmt(size),
+            '\t',
+            time_fmt(time_),
+        )
+        speeds.append(speed)
+
+    print('\n')
+    avg_speed = statistics.mean(speeds)
+    print(
+        "Average speed:",
+        sizeof_fmt(avg_speed) + '/s',
+    )
+    med_speed = statistics.median(speeds)
+    print(
+        "Median speed: ",
+        sizeof_fmt(med_speed) + '/s',
+    )
+
+
+if __name__ == '__main__':
+    run()
