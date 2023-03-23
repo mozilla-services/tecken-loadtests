d3754836fbfdb6934c3b706779236e449b75745e
Will Kahn-Greene <willkg@mozilla.com>
2023-03-23 14:14:34 -0400

    Update locust script to support multiple environments
    This adds the two GCP environments to the Eliot locust script.

--- locust_eliot.sh
+++ locust_eliot.sh
@@ -4,22 +4,50 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# Usage: ./molotov_eliot.sh [stage|prod]
+# Usage: ./locust_eliot.sh ENV [RUNNAME]
 #
 # Run it in the Docker container.
 
-DURATION=600
+AWS_STAGE_HOST="https://symbolication.stage.mozaws.net"
+AWS_PROD_HOST="https://symbolication.services.mozilla.com"
 
-STAGE_HOST="https://symbolication.stage.mozaws.net"
-PROD_HOST="https://symbolication.services.mozilla.com"
+GCP_STAGE_HOST="https://stage.eliot.nonprod.dataservices.mozgcp.net"
+GCP_PROD_HOST="https://prod.eliot.prod.dataservices.mozgcp.net"
 
-if [[ "$1" == "stage" ]]; then
-    export HOST=${STAGE_HOST}
-elif [[ "$1" == "prod" ]]; then
-    export HOST=${PROD_HOST}
+
+if [[ "$1" == "aws-stage" ]]; then
+    export HOST=${AWS_STAGE_HOST}
+
+elif [[ "$1" == "aws-prod" ]]; then
+    export HOST=${AWS_PROD_HOST}
+
+elif [[ "$1" == "gcp-stage" ]]; then
+    export HOST=${GCP_STAGE_HOST}
+
+elif [[ "$1" == "gcp-prod" ]]; then
+    export HOST=${GCP_PROD_HOST}
+
 else
-    echo "Unknown environment. Use 'stage' or 'prod'. Exiting."
+    echo "Unknown environment. Use 'aws-stage', 'aws-prod', 'gcp-stage', or 'gcp-prod'."
+    echo "Exiting."
     exit 1
 fi
 
-locust -f locust-eliot/testfile.py --host "${HOST}" --users 60 --run-time "10m" --print-stats --headless
+# Runname includes environment and any second argument
+RUNNAME="$(date +'%Y%m%d-%H0000')-$1$2"
+
+RUNTIME="5m"
+USERS="10"
+
+LOCUST_FLAGS="--print-stats --headless"
+
+echo ">>> Host:    ${HOST}"
+echo ">>> Runname: ${RUNNAME}"
+echo ">>> Locust start...."
+locust -f locust-eliot/testfile.py \
+    --host="${HOST}" \
+    --users="${USERS}" \
+    --run-time="${RUNTIME}" \
+    --csv="${RUNNAME}" \
+    ${LOCUST_FLAGS}
+echo ">>> Locust end."
