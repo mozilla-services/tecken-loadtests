dea03ad6796d6c77ad5736c67d982a85589eecc4
Will Kahn-Greene <willkg@mozilla.com>
2024-07-30 16:32:59 -0400

    Update requirements, remove cruft
    This removes molotov, anything that used it, Tecken system tests (now
    called smoke tests), and a bunch of other random things.
    
    This updates the code to use ruff for linting and formatting.

---
+++ GRAVEYARD.md
@@ -0,0 +1,13 @@
+# GRAVEYARD
+
+This is a file that links to interesting things in this repo in the past.
+
+## Molotov support and misc testing stuff
+
+* Removal PR
+
+A long time ago, we used [molotov](https://pypi.org/project/molotov/). We have
+since switched to [locust](https://pypi.org/project/locust/).
+
+Also, this repository has been used to hold Tecken system tests (now called
+smoke tests) and some other things. All of that was removed.
--- bin/run_lint.sh
+++ bin/run_lint.sh
@@ -3,15 +3,26 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Usage: bin/run_lint.sh [--fix]
 #
-# Usage: bin/run_lint.sh
+# Runs linting and code fixing.
+#
+# This should be called from inside a container.
 
-set -eo pipefail
+set -e
 
+FILES="bin locust*"
 PYTHON_VERSION=$(python --version)
 
-echo "black (${PYTHON_VERSION})"
-black $1 bin locust-eliot
+cd /app
 
-echo "ruff (${PYTHON_VERSION})"
-ruff bin locust-eliot
+if [[ $1 == "--format" ]]; then
+    echo ">>> ruff fix (${PYTHON_VERSION})"
+    ruff format $FILES
+    ruff check --fix $FILES
+else
+    echo ">>> ruff (${PYTHON_VERSION})"
+    ruff check $FILES
+    ruff format --check $FILES
+fi
